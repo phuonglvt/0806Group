6dc4fbaecca1d488e4de7e8d4bd60e62b2ea2256
Datpt0205 <86192047+Datpt0205@users.noreply.github.com>
2022-04-15 00:08:13 +0700

    enable extention php

--- app/Http/Controllers/ZipController.php
+++ app/Http/Controllers/ZipController.php
@@ -5,23 +5,39 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use ZipArchive;
 use File;
+use Illuminate\Support\Facades\Storage;
 
 class ZipController extends Controller
 {
     public function zipFile()
     {
-        $zip = new ZipArchive;
-        $fileName = 'myzip.zip';
+         $zip = new ZipArchive;
+         $fileName = 'MyZip.zip';
+        // if ($zip->open(public_path($fileName), ZipArchive::CREATE) === TRUE) {
+        //     foreach (Storage::files('public') as $name) {
+        //         $file = Storage::get($name);
+    
+        //         if ($name != 'public/.gitignore') {
+        //             $relativeNameInZipFile = basename($file);
+        //             $zip->addFile($file, $relativeNameInZipFile);
+        //         }
+        //     }
+        //     $zip->close();
+        // }
+        // return response()->download(public_path($fileName));
+        $path = public_path();
+        
         if($zip->open(public_path($fileName),ZipArchive::CREATE)===true)
         {
-            $files = File::files(public_path('myfiles'));
-            foreach($files as $key => $value){
-                $relativeNameInZipFile = basename($value);
+            
+            $file = File::files(public_path('storage\idea'));
+            foreach($file as $key => $value){
+                $relativeNameInZipFile = basename ($value); 
                 $zip->addFile($value,$relativeNameInZipFile);
-                }
-                $zip->close();
+            }
+            $zip->close(); 
         }
-            return response()->download(public_path($fileName));
+        return response()->download(public_path($fileName));
     }
 }
 
